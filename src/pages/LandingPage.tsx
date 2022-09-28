5c5e369d751e521a30067a6a159e333f32c94366
Tyrone <tyrone.cloma0219@gmail.com>
2022-09-28 12:33:13 -0400

    Using Codemirror6

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../styles/pages/LandingPage.module.scss';
 
 type Props = {}
 
 const LandingPage = (props: Props) => {
+   const [showDir, setShowDir] = useState<boolean>(false)
+   const [dirName, setDirName] = useState<string>('')
    // Defining hooks
    const navigate = useNavigate()
 
    return (
       <div className={styles.page}>
          <h1> Welcome to <span className={styles.titletext}>Hover</span> </h1>
-         <h6 onClick={() => navigate('/home')}> Click to continue </h6>
+         {showDir ?
+            <div className={styles.directory}>
+               <input
+                  type='text'
+                  placeholder='Enter file directory: '
+                  value={dirName}
+                  onChange={(e) => setDirName(e.target.value)}
+               />
+               <button onClick={() => {
+
+               }}> Browse </button>
+            </div>
+            :
+            <h6 onClick={() => setShowDir(!showDir)}> Click to continue </h6>
+         }
       </div >
    )
 }
